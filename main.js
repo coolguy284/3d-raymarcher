da08d20d1b30ba9a6768ce174d6f645bf1510c65
coolguy284 <coolguy284v64@gmail.com>
2023-12-11 06:40:13 -0500

    start making shader code
    Signed-off-by: coolguy284 <coolguy284v64@gmail.com>

--- main.js
+++ main.js
@@ -1,46 +1,65 @@
-let MAX_FRAME_TIME = 1 / 20;
+// js or shader
+let RENDER_PROFILE = 'js';
 
-let manager = new CanvasManager(canvas_container);
-let renderer = new Renderer3D();
-
-manager.setPixelRatio(10);
-manager.setDrawMode('2d');
-manager.setBackgroundColor('black');
-
-renderer.setDimensions(manager.getWidth(), manager.getHeight());
-renderer.setFov(pi * 0.5);
-renderer.setPosAndRot(0, 1, 0, 0, 0);
-
-let y = 0;
-
-let draw = async () => {
-  while (y < manager.getHeight()) {
-    let drawStartTime = Date.now();
+switch (RENDER_PROFILE) {
+  case 'js': {
+    let MAX_FRAME_TIME = 1 / 20;
     
-    for (; y < manager.getHeight(); y++) {
-      for (let x = 0; x < manager.getWidth(); x++) {
-        manager.setPixel(x, y, renderer.calculatePixelColor(x, y));
+    let manager = new CanvasManager(canvas_container);
+    
+    manager.setPixelRatio(10);
+    manager.setDrawMode('2d');
+    manager.setBackgroundColor('black');
+    
+    let renderer = new Renderer3D();
+    
+    renderer.setDimensions(manager.getWidth(), manager.getHeight());
+    renderer.setFov(pi * 0.5);
+    renderer.setPosAndRot(0, 1, 0, 0, 0);
+    
+    let y = 0;
+    
+    let draw = async () => {
+      while (y < manager.getHeight()) {
+        let drawStartTime = Date.now();
+        
+        for (; y < manager.getHeight(); y++) {
+          for (let x = 0; x < manager.getWidth(); x++) {
+            manager.setPixel(x, y, renderer.calculatePixelColor(x, y));
+          }
+          
+          if ((Date.now() - drawStartTime) > MAX_FRAME_TIME) {
+            break;
+          }
+        }
+        
+        setRenderProgress(y);
+        
+        await new Promise(r => requestAnimationFrame(r));
       }
       
-      if ((Date.now() - drawStartTime) > MAX_FRAME_TIME) {
-        break;
+      setRenderProgress(null);
+    };
+    
+    draw();
+    
+    function setRenderProgress(y) {
+      if (y == null) {
+        render_progress.textContent = '';
+      } else {
+        render_progress.textContent = `${y}/${manager.getHeight()}`;
       }
     }
-    
-    setRenderProgress(y);
-    
-    await new Promise(r => requestAnimationFrame(r));
+    break;
   }
   
-  setRenderProgress(null);
-};
-
-draw();
-
-function setRenderProgress(y) {
-  if (y == null) {
-    render_progress.textContent = '';
-  } else {
-    render_progress.textContent = `${y}/${manager.getHeight()}`;
-  }
+  case 'shader':
+    let manager = new CanvasManager();
+    
+    manager.setPixelRatio(2);
+    manager.setDrawMode('shader');
+    manager.setBackgroundColor('black');
+    
+    manager.draw();
+    break;
 }
