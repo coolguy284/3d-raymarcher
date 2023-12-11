0c827da7c7b762b1fc5734b379d224b5a6df3e08
coolguy284 <coolguy284v64@gmail.com>
2023-12-11 05:14:32 -0500

    create 3d renderer object
    Signed-off-by: coolguy284 <coolguy284v64@gmail.com>

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
 let MAX_FRAME_TIME = 1 / 20;
 
 let manager = new CanvasManager(canvas_container);
+let renderer = new Renderer3D();
 
 manager.setDrawMode('2d');
 
 manager.setBackgroundColor('black');
+
+renderer.setDimensions(manager.getWidth(), manager.getHeight());
 
 let y = 0;
 
@@ -13,7 +16,7 @@ let draw = () => {
   
   for (; y < manager.getHeight(); y++) {
     for (let x = 0; x < manager.getWidth(); x++) {
-      manager.setPixel(x, y, 'white');
+      manager.setPixel(x, y, renderer.calculatePixelColor(x, y));
     }
     
     if ((Date.now() - drawStartTime) > MAX_FRAME_TIME) {
@@ -21,7 +24,7 @@ let draw = () => {
     }
   }
   
-  requestAnimationFrame(draw);
+  if (y < manager.getHeight()) requestAnimationFrame(draw);
 };
 
 draw();
