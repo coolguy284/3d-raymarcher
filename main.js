fdffc2cf7234f281c3d56e45d0280183d6081d5e
coolguy284 <coolguy284v64@gmail.com>
2023-12-11 07:11:14 -0500

    add basic shader support
    Signed-off-by: coolguy284 <coolguy284v64@gmail.com>

--- main.js
+++ main.js
@@ -1,65 +1,67 @@
-// js or shader
-let RENDER_PROFILE = 'shader';
-
-switch (RENDER_PROFILE) {
-  case 'js': {
-    let MAX_FRAME_TIME = 1 / 20;
-    
-    let manager = new CanvasManager(canvas_container);
-    
-    manager.setPixelRatio(10);
-    manager.setDrawMode('2d');
-    manager.setBackgroundColor('black');
-    
-    let renderer = new Renderer3D();
-    
-    renderer.setDimensions(manager.getWidth(), manager.getHeight());
-    renderer.setFov(pi * 0.5);
-    renderer.setPosAndRot(0, 1, 0, 0, 0);
-    
-    let y = 0;
-    
-    let draw = async () => {
-      while (y < manager.getHeight()) {
-        let drawStartTime = Date.now();
-        
-        for (; y < manager.getHeight(); y++) {
-          for (let x = 0; x < manager.getWidth(); x++) {
-            manager.setPixel(x, y, renderer.calculatePixelColor(x, y));
+(async () => {
+  // js or shader
+  let RENDER_PROFILE = 'shader';
+  
+  switch (RENDER_PROFILE) {
+    case 'js': {
+      let MAX_FRAME_TIME = 1 / 20;
+      
+      let manager = new CanvasManager(canvas_container);
+      
+      manager.setPixelRatio(10);
+      await manager.setDrawMode('2d');
+      manager.setBackgroundColor('black');
+      
+      let renderer = new Renderer3D();
+      
+      renderer.setDimensions(manager.getWidth(), manager.getHeight());
+      renderer.setFov(pi * 0.5);
+      renderer.setPosAndRot(0, 1, 0, 0, 0);
+      
+      let y = 0;
+      
+      let draw = async () => {
+        while (y < manager.getHeight()) {
+          let drawStartTime = Date.now();
+          
+          for (; y < manager.getHeight(); y++) {
+            for (let x = 0; x < manager.getWidth(); x++) {
+              manager.setPixel(x, y, renderer.calculatePixelColor(x, y));
+            }
+            
+            if ((Date.now() - drawStartTime) > MAX_FRAME_TIME) {
+              break;
+            }
           }
           
-          if ((Date.now() - drawStartTime) > MAX_FRAME_TIME) {
-            break;
-          }
+          setRenderProgress(y);
+          
+          await new Promise(r => requestAnimationFrame(r));
         }
         
-        setRenderProgress(y);
-        
-        await new Promise(r => requestAnimationFrame(r));
-      }
+        setRenderProgress(null);
+      };
       
-      setRenderProgress(null);
-    };
-    
-    draw();
-    
-    function setRenderProgress(y) {
-      if (y == null) {
-        render_progress.textContent = '';
-      } else {
-        render_progress.textContent = `${y}/${manager.getHeight()}`;
+      draw();
+      
+      function setRenderProgress(y) {
+        if (y == null) {
+          render_progress.textContent = '';
+        } else {
+          render_progress.textContent = `${y}/${manager.getHeight()}`;
+        }
       }
+      break;
     }
-    break;
+    
+    case 'shader':
+      let manager = new CanvasManager(canvas_container);
+      
+      manager.setPixelRatio(2);
+      await manager.setDrawMode('shader');
+      manager.setBackgroundColor('black');
+      
+      manager.draw();
+      break;
   }
-  
-  case 'shader':
-    let manager = new CanvasManager(canvas_container);
-    
-    manager.setPixelRatio(2);
-    manager.setDrawMode('shader');
-    manager.setBackgroundColor('black');
-    
-    manager.draw();
-    break;
-}
+})();
---
+++ sr_fragment.glsl
@@ -0,0 +1,11 @@
+#version 300 es
+
+precision highp float;
+
+uniform vec2 iResolution;
+
+out vec4 outColor;
+
+void main() {
+  outColor = vec4(0.0, 1.0, 0.0, 1.0);
+}
